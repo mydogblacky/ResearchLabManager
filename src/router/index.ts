99ff4307e0e09162d5122c3fbf21e692d2c86e9c
kbombeke <bombeke.klaas@gmail.com>
2026-04-14 09:54:46 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@/components/Layout.vue'
 import Dashboard from '@/pages/Dashboard.vue'
 import Team from '@/pages/Team.vue'
@@ -8,7 +8,7 @@ import MeetingNotes from '@/pages/MeetingNotes.vue'
 import Settings from '@/pages/Settings.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
